2fafae61549b4cd09c92ea9b6478cc9a8d3187cb
KaMoMaKi <lukaknott49@gmail.com>
2025-05-27 11:48:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showLeds(`
-        . . . . .
-        . . . # .
-        . # . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.showString("hi!")
 })
-basic.showString("hi!")
 basic.forever(function () {
 	
 })
